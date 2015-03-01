a9628453e1b9b30346761ccaab3b7755fbc75e04
Varun Sharma <varun@sharmalabs.org>
2015-03-01 20:28:16 +0530

    Add setup.py and setup.cfg files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'django-static-media',
+    packages = ['django-static-media'],
+    version = '0.1',
+    description = 'Test',
+    license='MIT',
+    author = 'Varun Sharma',
+    author_email = 'varun@sharmalabs.org',
+    url = 'https://github.com/varunsharma/django-static-media',
+    download_url = 'https://github.com/varunsharma/django-static-media/tarball/0.1',
+    keywords = ['django', 'static', 'media'],
+    classifiers = [],
+)
